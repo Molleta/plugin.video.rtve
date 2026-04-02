d587b66ebeea79bd775090ba8af49cf527a2b0a4
Molleta <128399400+Molleta@users.noreply.github.com>
2026-04-02 18:53:12 +0200

    Refactor Kodi addon to handle categories and actions

--- main.py
+++ main.py
@@ -1,28 +1,89 @@
-# -*- coding: utf-8 -*-
-
-import xbmc
-import xbmcaddon
+import sys
+import xbmcplugin
 import xbmcgui
+import xbmcaddon
+from urllib.parse import parse_qs, urlencode
 
-__addon__ = xbmcaddon.Addon()
+# Get addon instance
+addon = xbmcaddon.Addon()
+handle = int(sys.argv[1])
+base_url = sys.argv[0]
 
-# Basic structure for a Kodi addon
-
-def list_videos():
-    videos = [
-        {'title': 'Video 1', 'url': 'http://example.com/video1'},
-        {'title': 'Video 2', 'url': 'http://example.com/video2'},
+def get_categories():
+    """Return list of main categories"""
+    return [
+        {
+            'name': 'En directo',
+            'icon': 'DefaultFolder.png',
+            'url': base_url + '?' + urlencode({'action': 'list_live'})
+        },
+        {
+            'name': 'A la carta',
+            'icon': 'DefaultFolder.png',
+            'url': base_url + '?' + urlencode({'action': 'list_vod'})
+        },
+        {
+            'name': 'Programas',
+            'icon': 'DefaultFolder.png',
+            'url': base_url + '?' + urlencode({'action': 'list_programs'})
+        }
     ]
-    return videos
 
+def list_categories():
+    """List main categories"""
+    categories = get_categories()
+    for cat in categories:
+        li = xbmcgui.ListItem(label=cat['name'])
+        li.setArt({'icon': cat['icon']})
+        xbmcplugin.addDirectoryItem(handle, cat['url'], li, isFolder=True)
+    xbmcplugin.endOfDirectory(handle)
+
+def list_live():
+    """List live channels"""
+    channels = [
+        {'title': 'La 1', 'url': 'http://example.com/la1'},
+        {'title': 'La 2', 'url': 'http://example.com/la2'},
+        {'title': '3/24', 'url': 'http://example.com/3-24'},
+    ]
+    for channel in channels:
+        li = xbmcgui.ListItem(label=channel['title'])
+        url = base_url + '?' + urlencode({'action': 'play', 'url': channel['url']})
+        xbmcplugin.addDirectoryItem(handle, url, li, isFolder=False)
+    xbmcplugin.endOfDirectory(handle)
+
+def list_vod():
+    """List on-demand content"""
+    programs = [
+        {'title': 'Programa 1', 'url': 'http://example.com/prog1'},
+        {'title': 'Programa 2', 'url': 'http://example.com/prog2'},
+    ]
+    for program in programs:
+        li = xbmcgui.ListItem(label=program['title'])
+        url = base_url + '?' + urlencode({'action': 'play', 'url': program['url']})
+        xbmcplugin.addDirectoryItem(handle, url, li, isFolder=False)
+    xbmcplugin.endOfDirectory(handle)
 
 def play_video(url):
-    xbmc.Player().play(url)
+    """Play video"""
+    li = xbmcgui.ListItem(path=url)
+    xbmcplugin.setResolvedUrl(handle, True, li)
+
+def router(params):
+    """Route actions"""
+    if not params:
+        list_categories()
+    else:
+        action = params.get('action', [''])[0]
+        if action == 'list_live':
+            list_live()
+        elif action == 'list_vod':
+            list_vod()
+        elif action == 'play':
+            url = params.get('url', [''])[0]
+            play_video(url)
+        else:
+            list_categories()
 
 if __name__ == '__main__':
-    # Example usage of the functions
-    videos = list_videos()
-    for video in videos:
-        print(video['title'])  # This would be displayed in the Kodi interface.
-        # Uncomment the line below to play video
-        # play_video(video['url'])
+    params = parse_qs(sys.argv[2].lstrip('?'))
+    router(params)
